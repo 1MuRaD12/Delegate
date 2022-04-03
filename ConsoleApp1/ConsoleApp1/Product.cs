d98faa5180adaa768e953ee5a6a1b1b160b4f0c4
1MuRaD12 <tu71tfsws@code.edu.az>
2022-04-03 22:33:35 +0300

    Delegate

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Product
+    {
+        public  void Total(double[] arr)
+        {
+            int count = default(int);
+            int total = default(int);
+            foreach (int num in arr)
+            {
+                count++;
+                total += num;
+            }
+            Console.WriteLine($"Total:{total}\nCount:{count}");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            double[] arr = { 5, 6, 4, 7, 10, 20, 30 };
+            Action<double[]> action = product.Total;
+            action(arr);
+
+        }
+
+    }
+}
